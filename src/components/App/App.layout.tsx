7fc6e9ecc7ebe6b5a4d15705b4add41221899ddd
Agnieszka Sierbista <agnieszka.sierbista@gmail.com>
2022-10-25 12:17:54 +0200

    Add basic components

---
+++ src/components/App/App.layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Header} from "../Header/Header.layout";
+import {Button} from "../Button/Button.layout";
+import {Input} from "../Input/Input.layout";
+
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Button/>
+            <Input/>
+        </div>
+    );
+}
+
+export default App;
